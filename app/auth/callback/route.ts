f2a7602bfae8d5a7fb495f25c17c4cc0ef32e0be
SH4M4NI4K <135929616+4lph4Omeg4@users.noreply.github.com>
2025-11-28 08:35:41 +0100

    Add Supabase middleware for OAuth callback page

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 
 export async function GET(request: Request) {
@@ -23,23 +23,7 @@ export async function GET(request: Request) {
 
     if (code) {
         const cookieStore = cookies()
-        const supabase = createServerClient(
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-            {
-                cookies: {
-                    get(name: string) {
-                        return cookieStore.get(name)?.value
-                    },
-                    set(name: string, value: string, options: CookieOptions) {
-                        cookieStore.set({ name, value, ...options })
-                    },
-                    remove(name: string, options: CookieOptions) {
-                        cookieStore.delete({ name, ...options })
-                    },
-                },
-            }
-        )
+        const supabase = createClient(cookieStore)
 
         const { error } = await supabase.auth.exchangeCodeForSession(code)
 
